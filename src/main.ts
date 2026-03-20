bacd610a2ed1232e5abab2d2da676b024f3d3258
01kevinms <kevin.victors29@gmail.com>
2026-03-19 22:37:45 -0300

    debug env

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,7 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 async function bootstrap() {
+  console.log('DATABASE_URL:', process.env.DATABASE_URL);
   const app = await NestFactory.create(AppModule,{
     cors:{
       origin:['https://interface-flowdo.vercel.app'],
