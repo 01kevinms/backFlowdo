bc4b20138c0ba684ba03684a66bcf7a6d124a3bf
01kevinms <kevin.victors29@gmail.com>
2026-03-19 21:25:06 -0300

    link

--- src/main.ts
+++ src/main.ts
@@ -7,7 +7,7 @@ import { ValidationPipe } from '@nestjs/common';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule,{
     cors:{
-      origin:['http://localhost:5176'],
+      origin:['https://interface-flowdo.vercel.app'],
       credentials:true,
       allowedHeaders:'Content-Type, Authorization',
       methods:'GET,POST,PUT,DELETE,PATCH',
